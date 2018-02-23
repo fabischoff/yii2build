674bbe118dc62bb831ae90b51fcdff80d3f06515
fausto <fausto@debianVM.detran.reders>
2018-02-23 11:57:37 -0300

    finalizado capitulo 5

--- common/models/User.php
+++ common/models/User.php
@@ -9,6 +9,10 @@ use yii\db\ActiveRecord;
 use yii\db\Expression;
 use yii\web\IdentityInterface;
 use yii\helpers\Security;
+use yii\helpers\ArrayHelper;
+use backend\models\Role;
+use yii\helpers\Url;
+use yii\helpers\Html;
 
 /**
  * User model
@@ -40,11 +44,25 @@ class User extends ActiveRecord implements IdentityInterface {
     /**
      * {@inheritdoc}
      */
+    public function attributeLabels() {
+        return [
+            /* Your other attribute labels */
+            'roleName' => Yii::t('app', 'Role'),
+            'statusName' => Yii::t('app', 'Status'),
+            'profileId' => Yii::t('app', 'Profile'),
+            'profileLink' => Yii::t('app', 'Profile'),
+            'userLink' => Yii::t('app', 'User'),
+            'username' => Yii::t('app', 'User'),
+            'userTypeName' => Yii::t('app', 'User Type'),
+            'userTypeId' => Yii::t('app', 'User Type'),
+            'userIdLink' => Yii::t('app', 'ID'),
+        ];
+    }
+
     public function behaviors() {
         return [
             'timestamp' => [
                 'class' => 'yii\behaviors\TimestampBehavior',
-                
                 'attributes' => [
                     ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
                     ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
@@ -59,22 +77,21 @@ class User extends ActiveRecord implements IdentityInterface {
      */
     public function rules() {
         return [
-        ['status_id', 'default', 'value' => self::STATUS_ACTIVE],
-        ['role_id', 'default', 'value' => 10],
-        ['user_type_id', 'default', 'value' => 10],
-            
-        ['username', 'filter', 'filter' => 'trim'],
-        ['username', 'required'],
-        ['username', 'unique'],
-        ['username', 'string', 'min' => 2, 'max' => 255],
-            
-        ['email', 'filter', 'filter' => 'trim'],
-        ['email', 'required'],
-        ['email', 'email'],
-        ['email', 'unique'],
+            ['status_id', 'default', 'value' => self::STATUS_ACTIVE],
+            [['status_id'], 'in', 'range' => array_keys($this->getStatusList())],
+            ['role_id', 'default', 'value' => 10],
+            ['user_type_id', 'default', 'value' => 10],
+            ['username', 'filter', 'filter' => 'trim'],
+            ['username', 'required'],
+            ['username', 'unique'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+            ['email', 'filter', 'filter' => 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'unique'],
+            [['role_id'], 'in', 'range' => array_keys($this->getRoleList())],
+            [['user_type_id'], 'in', 'range' => array_keys($this->getUserTypeList())]
         ];
-
-        
     }
 
     /**
@@ -108,14 +125,15 @@ class User extends ActiveRecord implements IdentityInterface {
      * @return static|null
      */
     public static function findByPasswordResetToken($token) {
-       $expire  = Yii::$app->params['user.passwordResetTokenExpire'];
-       $parts = explode('_', $token);
-       $timestamp = (int) end($parts);
-       
-       if($timestamp + $expire < time()){
-           //token expired
-           return null;
-       }
+        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+        $parts = explode('_', $token);
+        $timestamp = (int) end($parts);
+
+        if ($timestamp + $expire < time())
+        {
+//token expired
+            return null;
+        }
 
         return static::findOne([
                     'password_reset_token' => $token,
@@ -184,7 +202,7 @@ class User extends ActiveRecord implements IdentityInterface {
      * Generates "remember me" authentication key
      */
     public function generateAuthKey() {
-        $this->auth_key = Yii::$app->security->generateRandomString(). '_' . time();
+        $this->auth_key = Yii::$app->security->generateRandomString() . '_' . time();
     }
 
     /**
@@ -199,6 +217,125 @@ class User extends ActiveRecord implements IdentityInterface {
      */
     public function removePasswordResetToken() {
         $this->password_reset_token = null;
+    }
+
+    /**
+     * get role relationship
+     * 
+     */
+    public function getRole() {
+        return $this->hasOne(Role::className(), ['role_value' => 'role_id']);
+    }
+
+    /**
+     * get role name
+     * 
+     */
+    public function getRoleName() {
+        return $this->role ? $this->role->role_name : '- no role';
+    }
+
+    /**
+     * get list of roles for dropdown
+     */
+    public static function getRoleList() {
+        $droptions = Role::find()->asArray()->all();
+        return Arrayhelper::map($droptions, 'role_value', 'role_name');
+    }
+
+    /**
+     * get status relationship
+     *    
+     */
+    public function getStatus() {
+        return $this->hasOne(Status::className(), ['status_value', 'status_id']);
+    }
+
+    /**
+     *  get status name
+     *
+     */
+    public function getStatusName() {
+        return $this->status ? $this->status->status_name : '- no status -';
+    }
+
+    /**
+     * get list of statuses for dropdown
+     */
+    public static function getStatusList() {
+        $droptions = Status::find()->asArray()->all();
+        return ArrayHelper::map($droptions, 'status_value', 'status_name');
+    }
+
+    /**
+     * get getUserType relationship
+     *    
+     */
+    public function getUserType() {
+        return $this->hasOne(Status::className(), ['user_type_value', 'user_type_id']);
+    }
+
+    public function getUserTypeName() {
+        return $this->userType ? $this->userType->userType_name : '- no user type -';
+    }
+
+    public static function getUserTypeList() {
+        $droptions = userType::find()->asArray()->all();
+        return ArrayHelper::map($droptions, 'user_type_value', 'user_type_name');
+    }
+
+    /**
+     * get user type id
+     *
+     */
+    public function getUserTypeId() {
+        return $this->userType ? $this->userType->id : 'none';
+    }
+
+    /**
+     * @getProfile
+     *
+     */
+    public function getProfile() {
+        return $this->hasOne(Profile::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @getProfileId
+     *
+     */
+    public function getProfileId() {
+        return $this->profile ? $this->profile->id : 'none';
+    }
+
+    /**
+     * @getProfileLink
+     *
+     */
+    public function getProfileLink() {
+        $url = Url::to(['profile/view', 'id' => $this->profileId]);
+        $options = [];
+        return Html::a($this->profile ? 'profile' : 'none', $url, $options);
+    }
+
+    /**
+     * get user id Link
+     *
+     */
+    public function getUserIdLink() {
+        $url = Url::to(['user/update', 'id' => $this->id]);
+        $options = [];
+        return Html::a($this->id, $url, $options);
+    }
+
+    /**
+     * @getUserLink
+     *
+     */
+    public function getUserLink() {
+        $url = Url::to(['user/view', 'id' => $this->Id]);
+        $options = [];
+        return Html::a($this->username, $url, $options);
     }
 
 }
